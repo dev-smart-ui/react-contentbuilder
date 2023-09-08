4e42e342cf55661a97ea5d1f3bdaf174d2fce6ec
viktormoskalev07 <viktormoskalev07@gmail.com>
2023-09-08 10:43:15 +0300

    example

---
+++ example/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import {useEffect, useState} from "react";
+
+
+import axios from "axios";
+
+function App() {
+  const [dataFromCms , setDataFromCms]= useState("")
+  useEffect(()=>{
+    axios.get("https://builder.smart-ui.pro/load?page=%3Fpage%3DgoodPage").then(({data})=>{
+      setDataFromCms(data.html)
+    })
+  },[])
+
+  return (<>
+          <div className="container ">
+              <div dangerouslySetInnerHTML={{__html:dataFromCms}}/> }
+          </div>
+      </>
+
+  );
+}
+
+export default App;
