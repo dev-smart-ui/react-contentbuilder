d34e35cfc57dc8dae9da114945ae24459feab10c
TURP1 <turpicrypto@gmail.com>
2023-09-21 01:54:21 +0300

    change unworked tailwind properties to inline css

--- example/components/customBlocks/userInfo/userInfo.jsx
+++ example/components/customBlocks/userInfo/userInfo.jsx
@@ -5,8 +5,10 @@ export const UserInfo = () => {
 		<div className="w-100 h-16 row justify-start px-5 py-4 bg-gray-900 space-x-3 ">
 			<img className="w-12 h-12 rounded-full fill-current" src="" alt="" />
 			<div>
-				<div className="text-gray-400 text-xs font-normal leading-none ">Welcome back,</div>
-				<div className="text-white text-xl font-bold leading-relaxed">Joi Doo</div>
+				<div style={{fontSize: '12px' }}
+				 className="text-xs text-gray-400 font-normal leading-none ">Welcome back,</div>
+				<div style={{fontSize: '20px' }}
+				className="text-white text-xl font-bold leading-relaxed">Joi Doo</div>
 			</div>
 		</div>
 	)
@@ -19,10 +21,13 @@ export const builderUserInfo = {
 	'html':
 		`
 		<div class="w-100 h-16 row justify-start px-5 py-4 bg-gray-900 space-x-3 " data-custom="userInfo">
-			<img  style="width: 48px; height: 48px;" class="w-12 h-12 rounded-full" src="assets/minimalist-blocks/images/image-thumbnail.png" alt="" />
+			<img style="width: 48px; height: 48px;"
+			 class="w-12 h-12 rounded-full" src="assets/minimalist-blocks/images/image-thumbnail.png" alt="" />
 			<div>
-				<div class="text-gray-400 text-xs font-normal leading-none">Welcome back,</div>
-				<div class="text-white text-xl font-bold leading-relaxed">Joi Doo</div>
+				<div style="font-size: 12px;"
+				 class="text-gray-400 text-xs font-normal leading-none">Welcome back,</div>
+				<div style="font-size: 20px;"
+				 class="text-white text-xl font-bold leading-relaxed">Joi Doo</div>
 			</div>
 		</div>
 
--- example/components/customBlocks/watchList/watchList.jsx
+++ example/components/customBlocks/watchList/watchList.jsx
@@ -2,28 +2,37 @@
 
 export const WatchList = () => {
 	return (
-		<div className=" bg-gray-900 flex flex-col pt-5 px-5 pb-12">
-			<div className=" text-white text-xl font-bold leading-relaxed mb-5 ">Stocks Watchlist</div>
-			<div className=" w-100 h-16 border-l-3 border-solid border-teal-400 bg-black rounded-lg flex justify-between p-5 mb-2">
-				<div className="flex flex-col ">
-					<div className=" text-white text-base font-bold leading-tight">Kotak Group</div>
-					<div className=" text-gray-400 text-xs font-normal leading-none">Listed in BSE</div>
+		<div className=" bg-gray-900 flex flex-col pt-5 px-5 pb-12 ">
+
+			<div style={{ fontSize: '20px' }}
+				className=" text-white text-xl font-bold leading-relaxed mb-5 ">Stocks Watchlist</div>
+
+			<div style={{ borderLeft: '3px solid rgb(45 212 191)' }}
+				className=" w-100 h-18 border-l-3 border-solid border-teal-400 bg-black rounded-lg flex justify-between py-4 px-5 mb-2 ">
+				<div className=" flex flex-col ">
+					<div className=" text-white text-base font-bold leading-tight ">Kotak Group</div>
+					<div style={{ fontSize: '12px' }}
+						className=" text-gray-400 text-xs font-normal leading-tight ">Listed in BSE</div>
 				</div>
-				<div className="flex flex-col ">
+				<div className=" flex flex-col ">
 					<div className=" text-white text-base font-bold leading-tight text-right ">$14,691.25</div>
 					<div className=" text-teal-400 text-base font-bold leading-tight">+3.4 (0.91%)</div>
 				</div>
 			</div>
-			<div className=" w-100 h-16 border-l-3 border-solid border-red-500 bg-black rounded-lg flex justify-between p-5 mb-2">
-				<div className="flex flex-col ">
-					<div className=" text-white text-base font-bold leading-tight">Reliance Industries</div>
-					<div className=" text-gray-400 text-xs font-normal leading-none">Listed in BSE</div>
+
+			<div style={{ borderLeft: '3px solid rgb(239 68 68)' }}
+				className=" w-100 h-18 border-l-3 border-solid border-red-500 bg-black rounded-lg flex justify-between py-4 px-5 mb-2 ">
+				<div className=" flex flex-col ">
+					<div className=" text-white text-base font-bold leading-tight ">Reliance Industries</div>
+					<div style={{ fontSize: '12px' }}
+						className=" text-gray-400 text-xs font-normal leading-tight ">Listed in BSE</div>
 				</div>
-				<div className="flex flex-col ">
+				<div className=" flex flex-col ">
 					<div className=" text-white text-base font-bold leading-tight text-right ">$14,691.25</div>
-					<div className="  text-red-500 text-base font-bold  leading-tight">-4.4 (0.91%)</div>
+					<div className=" text-red-500 text-base font-bold leading-tight">-4.4 (0.91%)</div>
 				</div>
 			</div>
+
 		</div>
 	)
 }
@@ -34,28 +43,37 @@ export const builderWatchList = {
 	'category': '120',
 	'html':
 		`
-		<div class=" bg-gray-900 flex flex-col pt-5 px-5 pb-12" data-custom="watchList">
-			<div class=" text-white text-xl font-bold leading-relaxed mb-5 ">Stocks Watchlist</div>
-			<div class=" w-100 h-16 border-l-3 border-solid border-teal-400 bg-black rounded-lg flex justify-between p-5 mb-2">
-				<div class="flex flex-col ">
-					<div class=" text-white text-base font-bold leading-tight">Kotak Group</div>
-					<div class=" text-gray-400 text-xs font-normal leading-none">Listed in BSE</div>
+		<div class=" bg-gray-900 flex flex-col pt-5 px-5 pb-12 " data-custom="watchList">
+
+			<div style="font-size: 20px;"
+				class=" text-white text-xl font-bold leading-relaxed mb-5 ">Stocks Watchlist</div>
+
+			<div style="border-left: 3px solid rgb(45 212 191);"
+				class=" w-100 h-18 border-l-3 border-solid border-teal-400 bg-black rounded-lg flex justify-between py-4 px-5 mb-2 ">
+				<div class=" flex flex-col ">
+					<div class=" text-white text-base font-bold leading-tight ">Kotak Group</div>
+					<div style="font-size: 12px;"
+						class=" text-gray-400 text-xs font-normal leading-tight ">Listed in BSE</div>
 				</div>
-				<div class="flex flex-col ">
+				<div class=" flex flex-col ">
 					<div class=" text-white text-base font-bold leading-tight text-right ">$14,691.25</div>
 					<div class=" text-teal-400 text-base font-bold leading-tight">+3.4 (0.91%)</div>
 				</div>
 			</div>
-			<div class=" w-100 h-16 border-l-3 border-solid border-red-500 bg-black rounded-lg flex justify-between p-5 mb-2">
-				<div class="flex flex-col ">
-					<div class=" text-white text-base font-bold leading-tight">Reliance Industries</div>
-					<div class=" text-gray-400 text-xs font-normal leading-none">Listed in BSE</div>
+
+			<div style="border-left: 3px solid rgb(239 68 68);"
+				class=" w-100 h-18 border-l-3 border-solid border-red-500 bg-black rounded-lg flex justify-between py-4 px-5 mb-2 ">
+				<div class=" flex flex-col ">
+					<div class=" text-white text-base font-bold leading-tight ">Reliance Industries</div>
+					<div style="font-size: 12px;"
+						class=" text-gray-400 text-xs font-normal leading-tight ">Listed in BSE</div>
 				</div>
-				<div class="flex flex-col ">
+				<div class=" flex flex-col ">
 					<div class=" text-white text-base font-bold leading-tight text-right ">$14,691.25</div>
-					<div class="  text-red-500 text-base font-bold  leading-tight">-4.4 (0.91%)</div>
+					<div class=" text-red-500 text-base font-bold leading-tight">-4.4 (0.91%)</div>
 				</div>
 			</div>
+
 		</div>
 	`
 }
