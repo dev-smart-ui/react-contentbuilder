24bfcc00f497949220e3f4ac73fee75d6c7ae1eb
TURP1 <turpicrypto@gmail.com>
2023-09-20 19:35:38 +0300

    remove unused props

--- example/components/customBlocks/userInfo/userInfo.jsx
+++ example/components/customBlocks/userInfo/userInfo.jsx
@@ -1,11 +1,11 @@
 
 
-export const UserInfo = ({ image = '', firstName = 'Joi', lastName = 'Doo' }) => {
+export const UserInfo = () => {
 	return (
-		<div className="w-100 h-16 row justify-start px-5 bg-gray-900 gap-3 space-x-3 ">
-			<img className="w-12 h-12 rounded-full fill-current" src={"assets/minimalist-blocks/images/image-thumbnail.png"} alt="" />
-			<div className="">
-				<div className="text-gray-400 text-xs font-normal leading-4.05 roboto ">Welcome back,</div>
+		<div className="w-100 h-16 row justify-start px-5 py-4 bg-gray-900 space-x-3 ">
+			<img className="w-12 h-12 rounded-full fill-current" src="" alt="" />
+			<div>
+				<div className="text-gray-400 text-xs font-normal leading-none ">Welcome back,</div>
 				<div className="text-white text-xl font-bold leading-relaxed">Joi Doo</div>
 			</div>
 		</div>
@@ -21,7 +21,7 @@ export const builderUserInfo = {
 		<div class="w-100 h-16 row justify-start px-5 py-4 bg-gray-900 space-x-3 " data-custom="userInfo">
 			<img  style="width: 48px; height: 48px;" class="w-12 h-12 rounded-full" src="assets/minimalist-blocks/images/image-thumbnail.png" alt="" />
 			<div>
-				<div class="text-gray-400 text-xs  font-normal leading-none">Welcome back,</div>
+				<div class="text-gray-400 text-xs font-normal leading-none">Welcome back,</div>
 				<div class="text-white text-xl font-bold leading-relaxed">Joi Doo</div>
 			</div>
 		</div>
