e61a7b297d318035d6239a3010faed7937f113d0
TURP1 <turpicrypto@gmail.com>
2023-09-21 14:44:28 +0300

    change bg to gradient

--- example/components/customBlocks/inviteFriendCard/inviteFriendCard.jsx
+++ example/components/customBlocks/inviteFriendCard/inviteFriendCard.jsx
@@ -2,10 +2,10 @@
 
 export const InviteFriend = () => {
 	return (
-		<div className="px-6 py-2 bg-gray-900 relative">
+		<div className="px-6 py-2 bg-gray-900 relative" data-custom="inviteFriend">
 			<div
 				style={{
-					backgroundImage: "url('')",
+					background: "linear-gradient(104deg, #2885D4 9.91%, #063087 90.37%)",
 					backgroundSize: '100% 100%',
 					height: '162px',
 					backgroundRepeat: 'no-repeat',
@@ -16,7 +16,7 @@ export const InviteFriend = () => {
 			>
 				<div className="flex flex-col justify-between">
 					<div className="text-white text-base font-bold leading-tight">
-						Invite 1232 your friends to Finology and get Up to $100 benefits.
+						Invite your friends to Finology and get Up to $100 benefits.
 					</div>
 					<a href="#" className="flex space-x-2 text-cyan-500 no-underline">
 						<div style={{ fontSize: '12px' }} className="text-xs font-bold leading-tight">
@@ -36,7 +36,7 @@ export const InviteFriend = () => {
 						right: '-36px',
 						mixBlendMode: 'luminosity',
 					}}
-					src=""
+					src="assets/minimalist-blocks/images/pngegg-box.png"
 					alt=""
 				/>
 			</div>
@@ -52,8 +52,8 @@ export const builderInviteFriend = {
 	'html':
 		`
 			<div class="px-6 py-2 bg-gray-900 relative" data-custom="inviteFriend">
-				<div style="background-image: url('assets/minimalist-blocks/images/Rectangle-bg.png');background-size: 100% 100%; height: 162px; background-repeat: no-repeat; overflow: hidden; padding-right: 100px;"
-					class=" flex py-6 pl-6 bg-cover bg-center w-full relative rounded-xl ">
+				<div style="background: linear-gradient(104deg, #2885D4 9.91%, #063087 90.37%); height: 162px; overflow: hidden; padding-right: 100px;"
+					class="flex py-6 pl-6 bg-cover bg-center w-full relative rounded-xl">
 					<div class=" flex flex-col justify-between ">
 
 						<div class=" text-white text-base font-bold leading-tight ">
