e4f01003e969dba8a719c46bb5c1c5ca8666bc9e
TURP1 <turpicrypto@gmail.com>
2023-09-22 00:01:40 +0300

    refactor  marcup bunkCards for builder

--- example/components/customBlocks/bunkCards/bunkCards.jsx
+++ example/components/customBlocks/bunkCards/bunkCards.jsx
@@ -170,8 +170,8 @@ export const builderBunkCards = {
 		`
 		<div class=" flex flex-row py-2 px-6 justify-around bg-gray-900 space-x-2 " data-custom="bunkCards">
 
-			<div style=" width: 160px; height: 180px; position: relative; overflow: hidden;"
-				class=" flex flex-col px-3 py-4 items-start rounded-lg">
+			<div style=" position: relative; overflow: hidden;"
+				class=" flex flex-col px-3 py-4 items-start rounded-lg w-1/2">
 				<img src="assets/minimalist-blocks/images/Rectangle-bank-card-1.png"
 				 style="
 					position: absolute;
@@ -192,8 +192,8 @@ export const builderBunkCards = {
 			</div>
 
 			
-			<div style=" width: 160px; height: 180px; position: relative; overflow: hidden;"
-				class=" flex flex-col px-3 py-4 items-start rounded-lg">
+			<div style=" position: relative; overflow: hidden;"
+				class=" flex flex-col px-3 py-4 items-start rounded-lg w-1/2">
 
 				<img src="assets/minimalist-blocks/images/Rectangle-bank-card-2.png"
 				style="
