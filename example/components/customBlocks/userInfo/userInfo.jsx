d65868c7d7d03377181c4eb8475be40ecf88a2cd
TURP1 <turpicrypto@gmail.com>
2023-09-21 15:54:44 +0300

    fix row styles

--- example/components/customBlocks/userInfo/userInfo.jsx
+++ example/components/customBlocks/userInfo/userInfo.jsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 export const UserInfo = () => {
 	return (
-		<div className="w-100 h-16 row justify-start px-5 py-4 bg-gray-900 space-x-3 ">
+		<div className="w-100 h-16 flex justify-start px-5 py-4 bg-gray-900 space-x-3 ">
 			<Image width={48} height={48} className="w-12 h-12 rounded-full fill-current" src="" alt="" />
 			<div>
 				<div style={{fontSize: '12px' }}
@@ -21,7 +21,7 @@ export const builderUserInfo = {
 	'category': '120',
 	'html':
 		`
-		<div class="w-100 h-16 row justify-start px-5 py-4 bg-gray-900 space-x-3 " data-custom="userInfo">
+		<div class="w-100 h-16 flex justify-start px-5 py-4 bg-gray-900 space-x-3 " data-custom="userInfo">
 			<img style="width: 48px; height: 48px;"
 			 class="w-12 h-12 rounded-full" src="assets/minimalist-blocks/images/image-thumbnail.png" alt="" />
 			<div>
