7fdb210449fa3c14d23812cf6777cc01b34fd7f5
viktormoskalev07 <80700897+viktormoskalev07@users.noreply.github.com>
2023-10-25 17:08:14 +0300

    test

--- backend/getParams.js
+++ backend/getParams.js
@@ -3,26 +3,17 @@ const { JSDOM } = require('jsdom');
 
 const parse = async  ()=>{
 
-
-    const response = await fetch(`https://builder.smart-ui.pro/load?page=Test1`)
+    const response = await fetch(`https://builder.smart-ui.pro/load?page=user`)
     const data = await response.json()
     const html = data.html
 
-
-
     const dom = new JSDOM(html);
     const document = dom.window.document;
-    const images = document.querySelectorAll('img');
-    const texts = document.querySelectorAll('[data-textpropsname="mainText"]');
+    const elements = document.querySelectorAll('[data-component]');
 
-    images.forEach(img => {
-        console.log('src картинки:', img.src);
+    elements.forEach((element) => {
+        console.log(element);
     });
-
-    texts.forEach(text => {
-        console.log('Текст компонента:', text.textContent);
-    });
-
 }
 
 parse()
