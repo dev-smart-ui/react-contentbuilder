ab25c0d6c8e5f21827ecb623920d24a832fa02d0
webTime <webTime@gmail.com>
2023-11-01 17:13:25 +0200

    try fix getProps

--- backend/getProps.js
+++ backend/getProps.js
@@ -1,62 +1,64 @@
+// const { JSDOM } = require('jsdom');
+//
+// const getProps =    (html)=>{
+//     const dom = new JSDOM(html);
+//     const document = dom.window.document;
+//     const elements = document.querySelectorAll('[data-component]');
+//     const customProps={}
+//
+//     elements.forEach((element , i ) => {
+//         const componentType = element.getAttribute('data-component')||"type-error";
+//         customProps[componentType+""+i]={}
+//         const  editableNodes=  element.querySelectorAll('[data-propsname]');
+//         editableNodes.forEach((node  )=>{
+//             const propsName = node.getAttribute('data-propsname')||"propsname-error"
+//             try{
+//                 const fields = JSON.parse(propsName)
+//                 Object.entries(fields).forEach(field=>{
+//                     const propsName=field[0]
+//                     const propSource = field[1]
+//                     customProps[componentType+""+i][propsName]=node[propSource]
+//                 })
+//             } catch(e){
+//                 console.log(e)
+//             }
+//             // Object.entries()
+//             // if(componentType&&propsName&&source){
+//             //     customProps[componentType+""+i][propsName] = node[source]
+//             // }
+//         })
+//     });
+//
+//     return JSON.stringify(customProps)
+// }
+//
+//
+// module.exports = getProps
+
+
 const { JSDOM } = require('jsdom');
 
-const getProps =    (html)=>{
+const getProps = (html) => {
     const dom = new JSDOM(html);
     const document = dom.window.document;
-    const elements = document.querySelectorAll('[data-component]');
-    const customProps={}
+    const parentElement = document.querySelector('[data-component="Slider"]');
+    const childElementsWithProps = parentElement.querySelectorAll('[data-propsname]');
+    const propsList = [];
 
-    // elements.forEach((element, i) => {
-    //     const componentType = element.getAttribute('data-component') || "type-error";
-    //     if (!customProps[componentType]) {
-    //         customProps[componentType] = [];
-    //     }
-    //
-    //     const editableNodes = element.querySelectorAll('[data-propsname]');
-    //     editableNodes.forEach((node) => {
-    //         const propsName = node.getAttribute('data-propsname') || "propsname-error";
-    //
-    //         try {
-    //             const fields = JSON.parse(propsName);
-    //
-    //             console.log('fields ', fields)
-    //
-    //             fields.forEach((field) => {
-    //                 const propName = field[0];
-    //                 const propSource = field[1];
-    //                 customProps[componentType].push({ [propName]: node[propSource] });
-    //             });
-    //         } catch (e) {
-    //             console.log(e);
-    //         }
-    //     });
-    // });
-
-    elements.forEach((element , i ) => {
-        const componentType = element.getAttribute('data-component')||"type-error";
-        customProps[componentType+""+i]={}
-        const  editableNodes=  element.querySelectorAll('[data-propsname]');
-        editableNodes.forEach((node  )=>{
-            const propsName = node.getAttribute('data-propsname')||"propsname-error"
-            try{
-                const fields = JSON.parse(propsName)
-                Object.entries(fields).forEach(field=>{
-                    const propsName=field[0]
-                    const propSource = field[1]
-                    customProps[componentType+""+i][propsName]=node[propSource]
-                })
-            } catch(e){
-                console.log(e)
+    childElementsWithProps.forEach((element) => {
+        const propsName = element.getAttribute('data-propsname');
+        if (propsName) {
+            try {
+                const props = JSON.parse(propsName);
+                propsList.push(props);
+            } catch (e) {
+                console.log(e);
             }
-            // Object.entries()
-            // if(componentType&&propsName&&source){
-            //     customProps[componentType+""+i][propsName] = node[source]
-            // }
-        })
+        }
     });
 
-    return JSON.stringify(customProps)
+    return propsList;
 }
 
-
 module.exports = getProps
+
