bbccf36ebf8d8b9bcceaba3bfe44dc4eb377f5e0
viktormoskalev07 <80700897+viktormoskalev07@users.noreply.github.com>
2023-11-16 16:41:01 +0200

    autopush

--- razor-contentbuilder/src/components/exampleButton.tsx
+++ razor-contentbuilder/src/components/exampleButton.tsx
@@ -3,7 +3,7 @@ import {IsEditable} from "@components/config";
 
 const styles = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
 
-export const ExampleButton = ({text="placeholder"}) => {
+export const ExampleButton = ({text="placeholderText"}) => {
     return <button className={styles} data-component={"ExampleButton"}
                     {...IsEditable({ text: "textContent"})} >
           {text}
