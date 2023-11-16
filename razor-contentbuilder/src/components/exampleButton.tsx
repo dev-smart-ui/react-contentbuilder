ff741f7513f7032723b48c707eaa7d5379a4f0d3
viktormoskalev07 <80700897+viktormoskalev07@users.noreply.github.com>
2023-11-16 15:50:45 +0200

    autopush

--- razor-contentbuilder/src/components/exampleButton.tsx
+++ razor-contentbuilder/src/components/exampleButton.tsx
@@ -3,9 +3,9 @@ import {IsEditable} from "@components/config";
 
 
 export const ExampleButton = ({text="placeholder"}) => {
-
-    return <button  data-component={"ExampleButton"} {...IsEditable({ text: "textContent"})} >
-			{text}
+    return <button className="btn btn-blue" data-component={"ExampleButton"}
+                    {...IsEditable({ text: "textContent"})} >
+          {text}
     </button>
 };
 
